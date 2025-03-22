9e9aae670d36fbbfa82e5f899559bb6e9cc96c5f
kiyaku541 <95037284+kiyaku541@users.noreply.github.com>
2025-03-22 10:18:01 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>認証完了</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Bixet" />
+        <Header title="完了" />
         <p className="plane_text">
-          アプリを開いています。
+          認証完了です。
         </p>
       </main>
 
